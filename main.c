a8672f56818d944304feb7bf910b32678851a8f7
Joel INFELD <jinfeld@e1z3r3p5.42.us.org>
2017-08-01 13:47:03 -0700

    fdf

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include "mlx.h"
+#include "libft.h"
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
 int	main()
 {
-	void *mlx;
-	void *win;
+	//void *mlx;
+	//void *win;
+	int		fd;
+	char	*thestuff;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 400, 400, "mlx 42");
-	mlx_pixel_put(mlx, win, 200, 200, 0x00FFFFFF);
-	mlx_loop(mlx);
+	fd = open("42.fdf", O_RDONLY);
+	while (get_next_line(fd, &thestuff))
+	{
+		printf("%s\n", thestuff);
+	}
+	//mlx = mlx_init();
+	//win = mlx_new_window(mlx, 400, 400, "mlx 42");
+	//mlx_pixel_put(mlx, win, 200, 200, 0x00FFFFFF);
+	//mlx_loop(mlx);
 }
